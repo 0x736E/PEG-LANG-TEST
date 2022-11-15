67e041a54121c9c8625e6021c230caa16f70605f
seannicholls <seannicholls@gmail.com>
2022-11-15 20:58:08 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Introduction
+
+This repo isn't anything special, don't expect to find anything useful here.
+
+I am playing around with 'Parser Expression Language' to parse programming files, and putting those experiments here.
---
+++ test/ECMAScript/hello_world.js
@@ -0,0 +1,16 @@
+function myfunc ( var1 ) {
+
+    // this is a comment
+    var xyz = (123 + ('456' + 'string') * var1)
+
+    /*
+    test
+    */
+
+    if ( true ) {
+        // comment
+    } else ( false ) {
+        // another comment
+    }
+
+}
